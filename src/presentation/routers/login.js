5b98371cd8e85f944c09e0dbb894d317efca2e12
Mateus Lopes do Nascimento <mateus-lopes@outlook.com>
2021-02-13 12:30:10 -0300

    Ajustando as validaçoes de dependencias

--- src/presentation/routers/login.js
+++ src/presentation/routers/login.js
@@ -7,16 +7,20 @@ module.exports = class LoginRouter {
     };
 
     route(httpRequest) {
-        if (!httpRequest || !httpRequest.body || !this.authUseCase || !this.authUseCase.auth) return HttpResponse.serverError();
+        try {
+            const { body: { email, password } } = httpRequest;
 
-        const { body: { email, password } } = httpRequest;
+            if (!email) return HttpResponse.badRequest('email');
+            if (!password) return HttpResponse.badRequest('password');
 
-        if (!email) return HttpResponse.badRequest('email');
-        if (!password) return HttpResponse.badRequest('password');
-
-        const accessToken = this.authUseCase.auth(email, password);
-        if (!accessToken) return HttpResponse.unauthorized();
+            const accessToken = this.authUseCase.auth(email, password);
+            if (!accessToken) return HttpResponse.unauthorized();
         
-        return HttpResponse.ok({ accessToken });
+            return HttpResponse.ok({ accessToken });
+
+        } catch (error) {
+            return HttpResponse.serverError();
+        }
+        
     }
 }
