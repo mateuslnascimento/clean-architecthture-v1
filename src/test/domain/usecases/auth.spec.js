6e6addd4bfd63ff0437a720daa99a13cd0066ecf
Mateus Lopes do Nascimento <mateus-lopes@outlook.com>
2021-02-21 19:46:27 -0300

    Validando se o use Case esta chamando repositorio com o e-mail correto

--- src/test/domain/usecases/auth.spec.js
+++ src/test/domain/usecases/auth.spec.js
@@ -3,9 +3,15 @@
 const { MissingParamError } = require('../../../utils/errors')
 
 class AuthUseCase {
+  constructor (loadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
   async auth (email, password) {
     if (!email) throw new MissingParamError('email')
     if (!password) throw new MissingParamError('password')
+
+    await this.loadUserByEmailRepository.load(email, password)
   }
 }
 
@@ -20,5 +26,18 @@ describe('Auth UseCase', () => {
     const sut = new AuthUseCase()
     const promise = sut.auth('any_email@mail.com')
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  test('Should call LoadUserByEmailByRepository with correct email', async () => {
+    class LoadUserByEmailRepositorySpy {
+      async load (email) {
+        this.email = email
+      }
+    }
+
+    const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+    const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+    await sut.auth('any_email@mail.com', 'any_password')
+    expect(loadUserByEmailRepositorySpy.email).toBe('any_email@mail.com')
   })
 })
