70046e89f0f67960138f6d38b47e57762e19056b
Mateus Lopes do Nascimento <mateus-lopes@outlook.com>
2021-02-21 19:22:13 -0300

    Inserindo testes de validaçao de senha no metodo

--- src/test/domain/usecases/auth.spec.js
+++ src/test/domain/usecases/auth.spec.js
@@ -3,8 +3,9 @@
 const { MissingParamError } = require('../../../utils/errors')
 
 class AuthUseCase {
-  async auth (email) {
+  async auth (email, password) {
     if (!email) throw new MissingParamError('email')
+    if (!password) throw new MissingParamError('password')
   }
 }
 
@@ -13,5 +14,11 @@ describe('Auth UseCase', () => {
     const sut = new AuthUseCase()
     const promise = sut.auth()
     expect(promise).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  test('Should throw if no password is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@mail.com')
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 })
