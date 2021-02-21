7d1140285e0ca8fe8d5838d1fa5794484a2ea9f6
Mateus Lopes do Nascimento <mateus-lopes@outlook.com>
2021-02-21 19:20:06 -0300

    Validando erro para parametros faltantes no Auth Usecase

--- src/test/domain/usecases/auth.spec.js
+++ src/test/domain/usecases/auth.spec.js
@@ -1,15 +1,17 @@
 'use strict'
 
+const { MissingParamError } = require('../../../utils/errors')
+
 class AuthUseCase {
   async auth (email) {
-    if (!email) throw new Error()
+    if (!email) throw new MissingParamError('email')
   }
 }
 
 describe('Auth UseCase', () => {
-  test('Should thorw if no email is provided', async () => {
+  test('Should throw if no email is provided', async () => {
     const sut = new AuthUseCase()
     const promise = sut.auth()
-    expect(promise).rejects.toThrow()
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 })
